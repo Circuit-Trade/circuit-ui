665f1caf7c1018651bd4a7eee55a4b3991751b69
Chester Sim <chestersimdy@gmail.com>
2023-11-15 18:12:53 +0800

    refactor: table tabs

--- src/components/vaults/VaultTable/VaultTable.tsx
+++ src/components/vaults/VaultTable/VaultTable.tsx
@@ -6,7 +6,7 @@ import { BalancesTable } from './BalancesTable';
 import { OpenOrdersTable } from './OpenOrdersTable';
 import { OpenPositionsTable } from './OpenPositionsTable';
 
-const VAULT_TABLE_TABS = ['Orders', 'Positions', 'Balances'];
+const VAULT_TABLE_TABS = ['Positions', 'Balances', 'Orders'];
 
 export const VaultTable = () => {
 	const [selectedTab, setSelectedTab] = useState(VAULT_TABLE_TABS[0]);
